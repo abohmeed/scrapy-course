e30b0b6d84b590e31f3a74beef0db470e3416bf0
Ahmed Elfakharany <ahmed.elfakharany@kpn.com>
2022-03-28 16:31:47 +0200

    Creates a Scrapy skeleton project with spider

---
+++ projects/real_estate/real_estate/spiders/listings.py
@@ -0,0 +1,28 @@
+import scrapy
+from real_estate.items import RealEstateItem
+
+
+class ListingsSpider(scrapy.Spider):
+    name = 'listings'
+    allowed_domains = ['arizonarealestate.com']
+    start_urls = [
+        'https://www.arizonarealestate.com/maricopa/',
+        'https://www.arizonarealestate.com/goodyear/',
+        'https://www.arizonarealestate.com/tempe/'
+    ]
+
+    def parse(self, response):
+        gallery = response.xpath('//div[@class="si-listings-column"]')
+        for listing in gallery:
+            item = RealEstateItem()
+            # listing_details = dict()
+            item['name'] = listing.xpath(
+                './/div[@class="si-listing__title-main"]/text()').get()
+            item['description'] = listing.xpath(
+                './/div[@class="si-listing__title-description"]/text()').get()
+            item['price'] = listing.xpath(
+                './/div[@class="si-listing__photo-price"]/span/text()').get()
+            item['agency'] = listing.xpath(
+                './/div[@class="si-listing__footer"]/div/text()').get()
+            yield item
+            # self.found_listings.append(listing_details)
