bd7040f6caf1af786c4131c468f1c0427d13cb3d
Ahmed Elfakharany <ahmed.elfakharany@kpn.com>
2022-03-31 14:35:17 +0200

    Adds missing step

--- projects/real_estate/real_estate/spiders/listings.py
+++ projects/real_estate/real_estate/spiders/listings.py
@@ -19,6 +19,7 @@ class ListingsSpider(scrapy.Spider):
                 './/div[@class="si-listing__title-main"]/text() | .//div[@class="si-listing__neighborhood"]/span[@class="si-listing__neighborhood-place"]/text()').getall()
             item['description'] = listing.xpath(
                 './/div[@class="si-listing__info"]//div[@class="si-listing__info-label"]/text() | .//div[@class="si-listing__info"]//div[@class="si-listing__info-value"]/span/text()').getall()
+            item['description'] = [''.join(x.split()) for x in item['description']]
             item['price'] = listing.xpath(
                 './/div[@class="si-listing__photo-price"]/span/text()').get()
             item['agency'] = listing.xpath(
