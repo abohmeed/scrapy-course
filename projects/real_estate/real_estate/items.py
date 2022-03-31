63a9e277d3d8ae9e03d9929e8e5b46b22778b68c
Ahmed Elfakharany <ahmed.elfakharany@kpn.com>
2022-03-31 18:03:31 +0200

    Data cleansing using Item Loaders

--- projects/real_estate/real_estate/items.py
+++ projects/real_estate/real_estate/items.py
@@ -3,13 +3,30 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+from ntpath import join
 import scrapy
+from scrapy.loader.processors import Join, MapCompose, TakeFirst
+import json
+
+
+def description_in(d):
+    return d.strip()
+
+def description_out(d):
+    o = [0,3,1,4,2,5]
+    d = [d[i] for i in o]
+    it = iter(d)
+    # return json.dumps(dict(zip(it,it)))
+    return dict(zip(it,it))
 
 
 class RealEstateItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
-    description = scrapy.Field()
+    description = scrapy.Field(
+        input_processor=MapCompose(description_in),
+        # output_processor=Join()
+        output_processor=description_out
+    )
     price = scrapy.Field()
     agency = scrapy.Field()
-
--- projects/real_estate/real_estate/spiders/listings.py
+++ projects/real_estate/real_estate/spiders/listings.py
@@ -1,5 +1,6 @@
 import scrapy
 from real_estate.items import RealEstateItem
+from scrapy.loader import ItemLoader
 
 
 class ListingsSpider(scrapy.Spider):
@@ -14,14 +15,24 @@ class ListingsSpider(scrapy.Spider):
     def parse(self, response):
         gallery = response.xpath('//div[@class="si-listings-column"]')
         for listing in gallery:
-            item = RealEstateItem()
-            item['name'] = listing.xpath(
-                './/div[@class="si-listing__title-main"]/text() | .//div[@class="si-listing__neighborhood"]/span[@class="si-listing__neighborhood-place"]/text()').getall()
-            item['description'] = listing.xpath(
-                './/div[@class="si-listing__info"]//div[@class="si-listing__info-label"]/text() | .//div[@class="si-listing__info"]//div[@class="si-listing__info-value"]/span/text()').getall()
-            item['description'] = [''.join(x.split()) for x in item['description']]
-            item['price'] = listing.xpath(
-                './/div[@class="si-listing__photo-price"]/span/text()').get()
-            item['agency'] = listing.xpath(
-                './/div[@class="si-listing__footer"]/div/text()').get()
-            yield item
+            item = ItemLoader(item=RealEstateItem(),
+                              response=response, selector=listing)
+            item.add_xpath(
+                'name', './/div[@class="si-listing__title-main"]/text()'
+            )
+            item.add_xpath(
+                'name', './/div[@class="si-listing__neighborhood"]/span[@class="si-listing__neighborhood-place"]/text()'
+            )
+            item.add_xpath(
+                'description', './/div[@class="si-listing__info"]//div[@class="si-listing__info-label"]/text()'
+            )
+            item.add_xpath(
+                'description', './/div[@class="si-listing__info"]//div[@class="si-listing__info-value"]/span/text()'
+            )
+            item.add_xpath(
+                'price', './/div[@class="si-listing__photo-price"]/span/text()'
+            )
+            item.add_xpath(
+                'agency', './/div[@class="si-listing__footer"]/div/text()'
+            )
+            yield item.load_item()
