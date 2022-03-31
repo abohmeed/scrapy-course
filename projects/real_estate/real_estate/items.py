89429e8601d20619f5eee09b63ced4be678044c1
Ahmed Elfakharany <ahmed.elfakharany@kpn.com>
2022-03-31 18:03:31 +0200

    Data cleansing using Item Loaders

--- projects/real_estate/real_estate/items.py
+++ projects/real_estate/real_estate/items.py
@@ -4,12 +4,31 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import Join, MapCompose, TakeFirst
+import json
+
+
+def description_in(d):
+    return d.strip()
+
+
+def description_out(d):
+    labels = d[0:3]
+    values = d[3:]
+    output = {
+        labels[0]: values[0],
+        labels[1]: " ".join(values[1:-1]),
+        labels[2]: values[-1]
+    }
+    return output
 
 
 class RealEstateItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
-    description = scrapy.Field()
+    description = scrapy.Field(
+        input_processor=MapCompose(description_in),
+        output_processor=description_out
+    )
     price = scrapy.Field()
     agency = scrapy.Field()
-
