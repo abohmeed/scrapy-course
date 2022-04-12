9f13a5d8bc530e1e8b07ab0bcd9031982f3a8384
Ahmed Elfakharany <ahmed.elfakharany@kpn.com>
2022-04-12 11:06:38 +0200

    Adds the cahche policy

--- projects/naukri/naukri/settings.py
+++ projects/naukri/naukri/settings.py
@@ -84,8 +84,9 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 1000
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_POLICY = 'scrapy.extensions.httpcache.DummyPolicy'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
