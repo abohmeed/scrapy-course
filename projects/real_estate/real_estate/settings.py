2a8e88e4871c4bb981d3052a3223975ab06ea33e
Ahmed Elfakharany <ahmed.elfakharany@kpn.com>
2022-04-12 10:35:16 +0200

    Caching

--- projects/real_estate/real_estate/settings.py
+++ projects/real_estate/real_estate/settings.py
@@ -81,8 +81,8 @@ ROBOTSTXT_OBEY = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 1000
+HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
