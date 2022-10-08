ba1c607b0a233b3053a1228e000b6d614498fcda
Ahmed Elfakharany <abohmeed@gmail.com>
2022-10-08 09:18:45 +0200

    Resolves rebase conflicts

--- projects/classifieds/classifieds/pipelines.py
+++ projects/classifieds/classifieds/pipelines.py
@@ -6,8 +6,17 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
-class ClassifiedsPipeline:
+class ClassifiedsRemoveDuplicatesPipeline:
+    def __init__(self):
+        self.titles_seen = set()
+        
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        if adapter["title"] in self.titles_seen:
+            raise DropItem(f"Duplicate ad detected: {item}")
+        else:
+            self.titles_seen.add(adapter["title"])
         return item
