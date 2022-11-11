77c4799ce50e658d4b34fcfaceb6b30a50744dc7
Ahmed Elfakharany <abohmeed@gmail.com>
2022-11-11 21:47:48 +0100

    Creates the Yahoo Finane! project

---
+++ projects/yahoofinance/yahoofinance/spiders/stocks.py
@@ -0,0 +1,34 @@
+import scrapy
+from scrapy import Selector
+from itemloaders import ItemLoader
+from yahoofinance.items import YahoofinanceItem
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+class StocksSpider(scrapy.Spider):
+    name = 'stocks'
+    allowed_domains = ['yahoo.com']
+    start_urls = ['https://finance.yahoo.com/']
+
+    def parse(self, r):
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        driver.get(self.start_urls[0])
+        driver.implicitly_wait(2)
+        driver.find_element(By.NAME, "agree").click()
+        time.sleep(3)
+        driver.find_element(By.XPATH, "//button[@title='next']").click()
+        time.sleep(1)
+        driver.find_element(By.XPATH, "//button[@title='next']").click()
+        time.sleep(1)
+        response = driver.page_source
+        selector = Selector(text=response)
+        containers = selector.xpath("//li[contains(@id,'marketsummary-itm')]")
+        for c in containers:
+            item = ItemLoader(item=YahoofinanceItem(),response=response,selector=c)
+            item.add_xpath("name",".//a/text()")
+            item.add_xpath("change",".//fin-streamer/span/text()")
+            yield item.load_item()
+        driver.quit()
